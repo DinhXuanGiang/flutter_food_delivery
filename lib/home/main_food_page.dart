f7473565ae87e134f2f6970f7252fcf475da98a9
DinhXuanGiang <80635552+DinhXuanGiang@users.noreply.github.com>
2022-02-08 17:05:19 +0700

    draw the header section

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/widgets/big_text.dart';
+import 'package:food_delivery/widgets/small_text.dart';
+
+class MainHomePage extends StatefulWidget {
+  const MainHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MainHomePageState createState() => _MainHomePageState();
+}
+
+class _MainHomePageState extends State<MainHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            child: Container(
+              margin: EdgeInsets.only(top: 45, bottom: 15, ),
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      BigText(text: "Russia", color: AppColors.mainColor),
+                      Row(
+                        children: [
+                          SmallText(text: "Krasnoyarsk", color: Colors.black54),
+                          Icon(Icons.arrow_drop_down_rounded),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Center(
+                    child: Container(
+                      height: 45,
+                      width: 45,
+                      child: Icon(Icons.search, color: Colors.white),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: AppColors.mainColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
