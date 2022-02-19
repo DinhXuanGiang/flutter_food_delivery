0f0c36ddd7c183f7f08f98cc90e23786fa7d0c47
DinhXuanGiang <80635552+DinhXuanGiang@users.noreply.github.com>
2022-02-19 21:53:16 +0700

    add splashScreen

---
+++ lib/pages/splash/splash_page.dart
@@ -0,0 +1,61 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:food_delivery/controllers/popular_product_controller.dart';
+import 'package:food_delivery/controllers/recommended_product_controller.dart';
+import 'package:food_delivery/routes/route_helper.dart';
+import 'package:food_delivery/utils/dimensions.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_instance/src/extension_instance.dart';
+import 'package:get/get_navigation/src/extension_navigation.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin {
+  late Animation<double> animation;
+  late AnimationController animController;
+
+  Future<void> _loadRecources() async {
+    await Get.find<PopularProductController>().getPopularProductList();
+    await Get.find<RecommendedProductController>().getRecommendedProductList();
+  }
+
+  @override
+  void initState() {
+    _loadRecources();
+    animController = AnimationController(vsync: this, duration: const Duration(seconds: 2))..forward();
+    animation = CurvedAnimation(parent: animController, curve: Curves.linear);
+
+    Timer(
+      const Duration(seconds:3, ),
+            ()=>Get.offNamed(RouteHelper.getInitial())
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ScaleTransition(
+            scale: animation,
+            child: Center(
+              child: Image.asset("assets/image/logo part 1.png", width: Dimensions.splashImg),
+            ),
+          ),
+          Center(
+            child: Image.asset("assets/image/logo part 2.png", width: Dimensions.splashImg),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/utils/dimensions.dart
+++ lib/utils/dimensions.dart
@@ -1,47 +1,50 @@
 import 'package:get/get.dart';
-class Dimensions {
 
+class Dimensions {
   static double screenHeight = Get.context!.height;
   static double screenWidth = Get.context!.width;
 
-  static double pageView = screenHeight/2.67;
-  static double pageViewContainer = screenHeight/3.89;
-  static double pageViewTextContainer = screenHeight/7.13;
+  static double pageView = screenHeight / 2.67;
+  static double pageViewContainer = screenHeight / 3.89;
+  static double pageViewTextContainer = screenHeight / 7.13;
 
   //dynamic height padding and margin
-  static double height10 = screenHeight/85.6;
-  static double height15 = screenHeight/57.1;
-  static double height20 = screenHeight/42.8;
-  static double height30 = screenHeight/28.5;
-  static double height45 = screenHeight/19.03;
+  static double height10 = screenHeight / 85.6;
+  static double height15 = screenHeight / 57.1;
+  static double height20 = screenHeight / 42.8;
+  static double height30 = screenHeight / 28.5;
+  static double height45 = screenHeight / 19.03;
 
- //dynamic width padding and margin
-  static double width10 = screenHeight/85.6;
-  static double width15 = screenHeight/57.1;
-  static double width20 = screenHeight/42.8;
-  static double width30 = screenHeight/28.5;
-  static double width45 = screenHeight/19.03;
+  //dynamic width padding and margin
+  static double width10 = screenHeight / 85.6;
+  static double width15 = screenHeight / 57.1;
+  static double width20 = screenHeight / 42.8;
+  static double width30 = screenHeight / 28.5;
+  static double width45 = screenHeight / 19.03;
 
   //fontSize
-  static double font16 = screenHeight/53.5;
-  static double font20 = screenHeight/42.8;
-  static double font26 = screenHeight/32.95;
+  static double font16 = screenHeight / 53.5;
+  static double font20 = screenHeight / 42.8;
+  static double font26 = screenHeight / 32.95;
 
-  static double radius15 = screenHeight/57.1;
-  static double radius20 = screenHeight/42.8;
-  static double radius30 = screenHeight/28.5;
+  static double radius15 = screenHeight / 57.1;
+  static double radius20 = screenHeight / 42.8;
+  static double radius30 = screenHeight / 28.5;
 
   //icon Size
-  static double iconSize16 = screenHeight/53.54;
-  static double iconSize24 = screenHeight/35.69;
+  static double iconSize16 = screenHeight / 53.54;
+  static double iconSize24 = screenHeight / 35.69;
 
   //listview SizedBox
-  static double listViewImgSize = screenWidth/3.25;
-  static double listViewTextContSize = screenWidth/3.9;
+  static double listViewImgSize = screenWidth / 3.25;
+  static double listViewTextContSize = screenWidth / 3.9;
 
   //popular food
-  static double popularFoodImgSize = screenHeight/2.44;
+  static double popularFoodImgSize = screenHeight / 2.44;
 
   //bottom height
-static double bottomHeightBar = screenHeight/7.13;
+  static double bottomHeightBar = screenHeight / 7.13;
+
+  //splash screen dimensions
+  static double splashImg = screenHeight / 3.42;
 }
