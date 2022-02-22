0f84eb1f425dfc57f01c8e4f9a9b94e5b381a73a
DinhXuanGiang <80635552+DinhXuanGiang@users.noreply.github.com>
2022-02-23 03:17:47 +0700

    show empty history cart

--- lib/base/no_data_page.dart
+++ lib/base/no_data_page.dart
@@ -4,9 +4,12 @@ import 'package:flutter/material.dart';
 class NoDataPage extends StatelessWidget {
   final String text;
   final String imgPath;
-  const NoDataPage({Key? key, required this.text,
-        this.imgPath = "assets/image/empty_cart.png"
-  }) : super(key: key);
+
+  const NoDataPage(
+      {Key? key,
+      required this.text,
+      this.imgPath = "assets/image/empty_cart.png"})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,16 +17,18 @@ class NoDataPage extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Image.asset(imgPath,
-        height: MediaQuery.of(context).size.height*0.22,
-        width: MediaQuery.of(context).size.width*0.22,
+        Image.asset(
+          imgPath,
+          height: MediaQuery.of(context).size.height * 0.22,
+          width: MediaQuery.of(context).size.width * 0.22,
         ),
-        SizedBox(height: MediaQuery.of(context).size.height*0.03),
-        Text(text,
-        style: TextStyle(
-          fontSize: MediaQuery.of(context).size.height*0.0175,
-          color: Theme.of(context).disabledColor,
-        ),
+        SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+        Text(
+          text,
+          style: TextStyle(
+            fontSize: MediaQuery.of(context).size.height * 0.0175,
+            color: Theme.of(context).disabledColor,
+          ),
           textAlign: TextAlign.center,
         )
       ],
